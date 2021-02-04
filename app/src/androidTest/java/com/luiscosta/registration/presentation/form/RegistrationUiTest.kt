82d837a71e948400949353da93946904fe813c3a
luiscosta <luism.costa@thetrainline.com>
2021-02-04 00:52:48 +0000

    % Renames ui test class.

--- app/src/androidTest/java/com/luiscosta/registration/presentation/form/RegistrationUiTest.kt
+++ app/src/androidTest/java/com/luiscosta/registration/presentation/form/RegistrationUiTest.kt
@@ -15,7 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class FormFragmentUiTest {
+class RegistrationUiTest {
 
     @get:Rule
     var activityRule = ActivityTestRule(FormActivity::class.java, true, true)
